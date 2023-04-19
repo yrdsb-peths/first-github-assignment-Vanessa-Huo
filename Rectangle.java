e4c5215b51b7a70f9565da7b3d2770d0fcf62d41
Vanessa-Huo <130993636+Vanessa-Huo@users.noreply.github.com>
2023-04-19 08:34:29 -0400

    Create a 'Rectangle.java' file

---
+++ Rectangle.java
@@ -0,0 +1,22 @@
+public class Rectangle{
+  
+  private int length;
+  private int width; 
+  
+  public Rectangle(){
+    this.length = length;
+    this.width = width;
+  }
+  
+  public int getLength(){
+    return this.length; 
+  }
+  
+  public int getWidth(){
+    return this.width;
+  }
+  
+  public int getArea(){
+    return this.width * this.length; 
+  }
+}
